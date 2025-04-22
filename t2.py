eb85059f73e11325b763664cae38f9281a3779f4
LOVE KUMAR YADAV <123236986+love-0710@users.noreply.github.com>
2025-04-22 22:55:25 +0530

    Update t2.py

--- t2.py
+++ t2.py
@@ -1,1 +1,9 @@
-please use me to edit
+Set WshShell = CreateObject("WScript.Shell")
+WScript.Sleep 2000
+WshShell.SendKeys "C:\path\to\your\file.txt"
+WScript.Sleep 1000
+WshShell.SendKeys "{ENTER}"
+
+
+
+"wscript C:/path/to/fileUpload.vbs".execute().waitFor()
