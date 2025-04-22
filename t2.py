ddb17367f9b38cc3558f5f1ea4ddbf602b972bc6
LOVE KUMAR YADAV <123236986+love-0710@users.noreply.github.com>
2025-04-22 23:26:06 +0530

    Update t2.py

--- t2.py
+++ t2.py
@@ -1,9 +1,23 @@
-Set WshShell = CreateObject("WScript.Shell")
-WScript.Sleep 2000
-WshShell.SendKeys "C:\path\to\your\file.txt"
-WScript.Sleep 1000
-WshShell.SendKeys "{ENTER}"
+If isValid Then
+    WScript.Sleep 2000
+    WshShell.SendKeys filePath
+    WScript.Sleep 1000
+    WshShell.SendKeys "{ENTER}"
+Else
+    WScript.Quit 1  ' Exit with failure code
+End If
 
 
 
-"wscript C:/path/to/fileUpload.vbs".execute().waitFor()
+
+def proc = "wscript C:/path/to/upload.vbs".execute()
+int exitCode = proc.waitFor()
+
+if (exitCode != 0) {
+    log.warn("Invalid file type. Upload aborted.")
+    SampleResult.setSuccessful(false)
+    SampleResult.setResponseMessage("Upload failed: Invalid file type.")
+} else {
+    log.info("File uploaded successfully.")
+    SampleResult.setSuccessful(true)
+}
