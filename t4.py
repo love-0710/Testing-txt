c1c8f1339643efd798f596df23312fbf4fecff4b
LOVE KUMAR YADAV <123236986+love-0710@users.noreply.github.com>
2025-04-24 16:23:28 +0530

    Update t4.py

--- t4.py
+++ t4.py
@@ -1,4 +1,38 @@
 Set WshShell = CreateObject("WScript.Shell")
+Set fso = CreateObject("Scripting.FileSystemObject")
+
+allowedExts = Array("txt", "pdf", "xls", "doc", "xlsx", "docx", "ppt", "pptx", "rtf", "zip", "xml")
+
+For i = 0 To WScript.Arguments.Count - 1
+    filePath = WScript.Arguments(i)
+    fileExt = LCase(fso.GetExtensionName(filePath))
+    isValid = False
+
+    For Each ext In allowedExts
+        If fileExt = ext Then
+            isValid = True
+            Exit For
+        End If
+    Next
+
+    If isValid Then
+        WScript.Sleep 1000
+        WshShell.SendKeys filePath
+        WScript.Sleep 1000
+        WshShell.SendKeys "{ENTER}"
+    Else
+        WScript.Quit 1 ' Invalid file extension
+    End If
+Next
+
+
+
+
+
+
+
+
+Set WshShell = CreateObject("WScript.Shell")
 Set args = WScript.Arguments
 
 For i = 0 To args.Count - 1
