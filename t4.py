cbb396f13ec74aa1159b6a1f6f694dacc100ff71
LOVE KUMAR YADAV <123236986+love-0710@users.noreply.github.com>
2025-05-05 13:46:59 +0530

    Update t4.py

--- t4.py
+++ t4.py
@@ -1,125 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
 
-Set WshShell = CreateObject("WScript.Shell")
-Set fso = CreateObject("Scripting.FileSystemObject")
+:: Change directory to JMeter bin
+cd /d "C:\Users\practice_project\jmeter\apache-jmeter-5.6.3\bin"
 
-allowedExts = Array("txt", "pdf", "xls", "doc", "xlsx", "docx", "ppt", "pptx", "rtf", "zip", "xml")
-fileString = ""
+:: List of script names (without extension)
+set scripts=interactiveview loginFlow paymentTest searchTest checkoutFlow
 
-' Validate each file and build the input string
-For i = 0 To WScript.Arguments.Count - 1
-    filePath = WScript.Arguments(i)
-    fileExt = LCase(fso.GetExtensionName(filePath))
-    isValid = False
+:: Loop through each script
+for %%s in (%scripts%) do (
+    call :runScript %%s
+)
 
-    For Each ext In allowedExts
-        If fileExt = ext Then
-            isValid = True
-            Exit For
-        End If
-    Next
+goto :eof
 
-    If Not isValid Then
-        WScript.Quit 1 ' Exit if any file is invalid
-    End If
+:runScript
+setlocal
+set "scriptName=%~1"
 
-    ' Add quotes around each file path
-    If i = 0 Then
-        fileString = Chr(34) & filePath & Chr(34)
-    Else
-        fileString = fileString & " " & Chr(34) & filePath & Chr(34)
-    End If
-Next
+:: Generate timestamp
+set "timestamp=%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
+set "timestamp=%timestamp: =0%"
 
-' Send the full quoted string at once
-WScript.Sleep 1000
-WshShell.SendKeys fileString
-WScript.Sleep 1000
-WshShell.SendKeys "{ENTER}"
+echo Running %scriptName% at %timestamp%
 
+:: Run JMeter command
+jmeter -n -t "C:\Users\practice_project\JMeterMySS\JMeterScripts\%scriptName%.jmx" ^
+ -l "C:\Users\practice_project\JMeterMySS\JMeterLogs\%scriptName%_%timestamp%_Report.csv" ^
+ -e -o "C:\Users\practice_project\JMeterMySS\JMeterReports\%scriptName%_%timestamp%_Report"
 
-
-
-
-
-
-
-
-
-
-
-// Combine both file paths in double quotes separated by space
-var fullPath = '"' + file1 + '" "' + file2 + '"';
-
-// Call VBScript to handle file dialog
-var vbsPath = "C:\\Users\\YourName\\upload.vbs";
-var command = 'wscript "' + vbsPath + '" "' + fullPath + '"';
-
-WDS.log.info("Executing command: " + command);
-
-
-
-
-
-Set WshShell = CreateObject("WScript.Shell")
-Set fso = CreateObject("Scripting.FileSystemObject")
-
-allowedExts = Array("txt", "pdf", "xls", "doc", "xlsx", "docx", "ppt", "pptx", "rtf", "zip", "xml")
-
-For i = 0 To WScript.Arguments.Count - 1
-    filePath = WScript.Arguments(i)
-    fileExt = LCase(fso.GetExtensionName(filePath))
-    isValid = False
-
-    For Each ext In allowedExts
-        If fileExt = ext Then
-            isValid = True
-            Exit For
-        End If
-    Next
-
-    If isValid Then
-        WScript.Sleep 1000
-        WshShell.SendKeys filePath
-        WScript.Sleep 1000
-        WshShell.SendKeys "{ENTER}"
-    Else
-        WScript.Quit 1 ' Invalid file extension
-    End If
-Next
-
-
-
-
-
-
-
-
-Set WshShell = CreateObject("WScript.Shell")
-Set args = WScript.Arguments
-
-For i = 0 To args.Count - 1
-    WScript.Sleep 1000
-    WshShell.SendKeys args(i)
-    WScript.Sleep 1000
-    WshShell.SendKeys "{ENTER}"
-Next
-
-
-
-
-
-var vbs = WDS.vars.get("vbsPath");
-var file1 = WDS.vars.get("filepath1");
-var file2 = WDS.vars.get("filepath2");
-
-// Add quotes to file paths
-var quotedFile1 = '"' + file1 + '"';
-var quotedFile2 = '"' + file2 + '"';
-
-// Build the command
-var command = 'wscript "' + vbs + '" ' + quotedFile1 + ' ' + quotedFile2;
-WDS.log.info("Executing: " + command);
-
-// Execute
-var proc = java.lang.Runtime.getRuntime().exec(command);
-var exitCode = proc.waitFor();
+endlocal
+exit /b
