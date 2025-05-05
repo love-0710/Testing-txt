66dd5518ff98714047366c1de42af264ad6be025
LOVE KUMAR YADAV <123236986+love-0710@users.noreply.github.com>
2025-05-05 13:38:28 +0530

    Update t3.py

--- t3.py
+++ t3.py
@@ -4,23 +4,22 @@ setlocal enabledelayedexpansion
 :: Change directory to JMeter bin
 cd /d "C:\Users\practice_project\jmeter\apache-jmeter-5.6.3\bin"
 
-:: List of script names (without extension)
+:: List of script names
 set scripts=interactiveview loginFlow paymentTest searchTest checkoutFlow
 
-:: Loop through each script and call subroutine
+:: Loop through each script name
 for %%s in (%scripts%) do (
     call :runScript %%s
 )
 
-goto :end
+goto :eof
 
-:: Subroutine to run JMeter script
 :runScript
-set scriptName=%1
+set "scriptName=%1"
 
-:: Get timestamp
-set timestamp=%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-set timestamp=%timestamp: =0%
+:: Generate a clean timestamp
+set "timestamp=%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%"
+set "timestamp=%timestamp: =0%"
 
 echo Running %scriptName%...
 
@@ -30,11 +29,6 @@ jmeter -n -t "C:\Users\practice_project\JMeterMySS\JMeterScripts\%scriptName%.jm
 
 echo Done with %scriptName%
 exit /b
-
-:end
-endlocal
-pause
-
 
 
 
